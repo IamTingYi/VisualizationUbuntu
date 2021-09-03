013b79083132a4995aaa1334b77ab6b18ed0a254
IamTingYi <sophia880528@gmail.com>
2021-09-04 01:46:40 +0800

    commit_0904

--- shiny_app/server.R
+++ shiny_app/server.R
@@ -1,17 +1,109 @@
-library(shiny)
+dataPractice <- airquality
 
-# Define server logic required to draw a histogram
-shinyServer(function(input, output) {
-   
-  output$distPlot <- renderPlot({
-    
-    # generate bins based on input$bins from ui.R
-    x    <- faithful[, 2] 
-    bins <- seq(min(x), max(x), length.out = input$bins + 1)
-    
-    # draw the histogram with the specified number of bins
-    hist(x, breaks = bins, col = 'darkgray', border = 'white')
-    
+shinyServer(function(input, output, session) {
+  ##### Textbook Page #####
+  output$pdf1 <- renderUI({
+    tags$iframe(style="height:1000px; width:100%", src="1單元主題簡介.pdf")
+  })
+  output$pdf2 <- renderUI({
+    tags$iframe(style="height:1000px; width:100%", src="2資料視覺化介紹.pdf")
+  })
+  output$pdf3 <- renderUI({
+    tags$iframe(style="height:1000px; width:100%", src="3常見視覺化圖表與程式實作.pdf")
+  })
+  output$pdf4 <- renderUI({
+    tags$iframe(style="height:1000px; width:100%", src="4政府應用實例.pdf")
+  })
+  output$html1 <- renderUI({
+    tags$iframe(src = "example.html", width = "100%", height = "1000", seamless=TRUE)
   })
   
+  ##### Data Set Page #####
+  var <- reactiveValues(table = dataPractice)
+  observeEvent(input$start1,{
+    dataSet <- switch(input$dataSet,
+                      "airquality" = airquality,
+                      "diamonds" = diamonds,
+                      "iris" = iris,
+                      "midwest" = midwest,
+                      "mpg" = mpg,
+                      "mtcars" = mtcars)
+    if(class(dataSet)[1]!="data.frame"){
+      dataSet <- as.data.frame(dataSet)
+    }
+    var$table <- dataSet
+    updateSelectInput(session, "bar_feature", label = "Feature", choices = colnames(var$table))
+    updateSelectInput(session, "bar_color", label = "Color", choice = colnames(var$table))
+    updateSelectInput(session, "hist_feature", label = "Feature", choices = colnames(var$table))
+    updateSelectInput(session, "hist_color", label = "Color", choices = colnames(var$table))
+    updateSelectInput(session, "pie_feature", label = "Feature", choices = colnames(var$table))
+    updateSelectInput(session, "scatter_x", label = "Var X", choices = colnames(var$table))
+    updateSelectInput(session, "scatter_y", label = "Var Y", choices = colnames(var$table))
+    updateSelectInput(session, "scatter_color", label = "Color", choices = colnames(var$table))
+  })
+  observeEvent(input$start2,{
+    req(input$dataUpload)
+    intial_data <- read.csv(input$dataUpload$datapath, header = TRUE)
+    var$table <- intial_data
+    updateSelectInput(session, "bar_feature", label = "Feature", choices = colnames(var$table))
+    updateSelectInput(session, "bar_color", label = "Color", choice = colnames(var$table))
+    updateSelectInput(session, "hist_feature", label = "Feature", choices = colnames(var$table))
+    updateSelectInput(session, "hist_color", label = "Color", choices = colnames(var$table))
+    updateSelectInput(session, "pie_feature", label = "Feature", choices = colnames(var$table))
+    updateSelectInput(session, "scatter_x", label = "Var X", choices = colnames(var$table))
+    updateSelectInput(session, "scatter_y", label = "Var Y", choices = colnames(var$table))
+    updateSelectInput(session, "scatter_color", label = "Color", choices = colnames(var$table))
+  })
+  output$dataTable <- renderDataTable({var$table})
+  
+  ##### Data Summary Page #####
+  output$summary <- renderPrint({summary(var$table)})
+  
+  ##### Plot Page #####
+  output$bar_plot <- renderPlot({
+    feature <- var$table[, input$bar_feature]
+    color <- var$table[, input$bar_color]
+    ggplot(data = var$table, mapping = aes(x = feature, fill = color)) +
+      geom_bar() +
+      labs(x = input$bar_feature, y = "Frequency") +
+      theme_minimal()
+  })
+  output$hist_plot <- renderPlot({
+    feature <- var$table[, input$hist_feature]
+    color <- var$table[, input$hist_color]
+    ggplot(data = var$table, mapping = aes(x = feature, fill = color)) +
+      geom_histogram(bins = 30) +
+      labs(x = input$hist_feature, y = "Frequency") +
+      theme_minimal() 
+  })
+  output$pie_plot <- renderPlot({
+    feature <- var$table[, input$pie_feature]
+    ggplot(data = var$table, aes(x = "", fill = factor(feature))) +
+      geom_bar(width = 1) +
+      coord_polar(theta = "y") +
+      theme_void()
+  })
+  output$scatter_plot <- renderPlot({
+    x <- var$table[, input$scatter_x]
+    y <- var$table[, input$scatter_y]
+    color <- var$table[, input$scatter_color]
+    ggplot(data = var$table, mapping = aes(x, y, color = color)) + 
+      geom_point(size = 5, alpha = 0.5) +
+      scale_x_continuous(input$scatter_x) +
+      scale_y_continuous(input$scatter_y) +
+      theme_minimal()
+  })
+  
+  ##### Practice Page #####
+  output$pdf5 <- renderUI({
+    tags$iframe(style="height:1000px; width:100%", src="5練習-資料視覺化實例操作.pdf")
+  })
+  output$html2 <- renderUI({
+    tags$iframe(src = "example.html", width = "100%", height = "1000", seamless=TRUE)
+  })
+  
+  ##### Code Page #####
+  output$jupyter <- renderUI({
+    tags$iframe(src = "http://localhost:8888/?token=695ebae5b3a929335d970cb6aa57e366fc2ce8525662a94e", width = "100%", height = "1000")
+  })
 })
--- shiny_app/ui.R
+++ shiny_app/ui.R
@@ -1,24 +1,231 @@
 library(shiny)
+library(shinydashboard)
+library(ggplot2)
+library(dplyr)
 
-# Define UI for application that draws a histogram
-shinyUI(fluidPage(
-  
-  # Application title
-  titlePanel("Old Faithful Geyser Data"),
-  
-  # Sidebar with a slider input for number of bins 
-  sidebarLayout(
-    sidebarPanel(
-       sliderInput("bins",
-                   "Number of bins:",
-                   min = 1,
-                   max = 50,
-                   value = 30)
-    ),
-    
-    # Show a plot of the generated distribution
-    mainPanel(
-       plotOutput("distPlot")
-    )
-  )
-))
+data("airquality")
+data("diamonds")
+data("iris")
+data("midwest")
+data("mpg")
+data("mtcars")
+
+dataPractice <- airquality
+
+dashboardPage(skin = "green",
+              
+              dashboardHeader(
+                title = "Data Visualization"
+              ),
+              
+              dashboardSidebar(
+                sidebarMenu(
+                  menuItem("Textbook", tabName = "Textbook", icon = icon("book")),
+                  menuItem("Data Set", tabName = "Data", icon = icon("th")),
+                  menuItem("Data Summary", tabName = "Summary", icon = icon("table")),
+                  menuItem("Plot", tabName = "Plot", icon = icon("image"),
+                           menuItem("Bar Chart", tabName = "Bar", icon = icon("chart-bar")),
+                           menuItem("Histogram", tabName = "Hist", icon = icon("chart-bar")),
+                           menuItem("Pie Chart", tabName = "Pie", icon = icon("chart-pie")),
+                           menuItem("Scatter Plot", tabName = "Scatter", icon = icon("spinner"))),
+                  menuItem("Practice", tabName = "Practice", icon = icon("edit")),
+                  menuItem("Code", tabName = "Code")
+                )
+              ),
+              
+              dashboardBody(
+                tabItems(
+                  ##### Textbook Page #####
+                  tabItem(
+                    tabName = "Textbook",
+                    fluidPage(
+                      tabBox(
+                        width = 7,
+                        height = 1100,
+                        tabPanel(
+                          title = "單元主題簡介",
+                          uiOutput("pdf1")
+                        ),
+                        tabPanel(
+                          title = "資料視覺化介紹",
+                          uiOutput("pdf2")
+                        ),
+                        tabPanel(
+                          title = "常見視覺化圖表與程式實作",
+                          uiOutput("pdf3")
+                        ),
+                        tabPanel(
+                          title = "政府應用實例",
+                          uiOutput("pdf4")
+                        )
+                      ),
+                      box(
+                        title = "R Code Practice",
+                        solidHeader = TRUE,
+                        status = "primary",
+                        width=5,
+                        height = 1100,
+                        htmlOutput("html1"))
+                    )
+                  ),
+                  
+                  ##### Data Set Page #####
+                  tabItem(
+                    tabName = "Data",
+                    fluidPage(
+                      tabBox(
+                        title = strong("Data Set"),
+                        width = 12,
+                        tabPanel(
+                          title = "Select",
+                          icon = icon("list"),
+                          selectInput("dataSet", label = "Select Data Set",
+                                      choices = c("airquality","diamonds","iris","midwest","mpg","mtcars")),
+                          actionButton("start1", "Select", icon = icon("arrow-alt-circle-right"))),
+                        tabPanel(
+                          title = "Upload",
+                          icon = icon("file-csv"),
+                          fileInput("dataUpload", "Upload Data Set",
+                                    accept = c("text/csv", "text/comma-separated-values", ".csv")),  
+                          tags$script("$('#dataUpload').on('click', function() { this.value = null; });"),
+                          actionButton("start2", "Upload", icon = icon("eject")))
+                      ),
+                      box(
+                        title = "Data Table",
+                        solidHeader = TRUE,
+                        status = "primary",
+                        width = 12,
+                        dataTableOutput("dataTable")
+                      )
+                    )
+                  ),
+                  
+                  ##### Data Summary Page #####
+                  tabItem(
+                    tabName = "Summary",
+                    fluidPage(
+                      box(
+                        title = "Data Summary",
+                        solidHeader = TRUE,
+                        status = "primary",
+                        width = 12,
+                        verbatimTextOutput("summary")
+                      )
+                    )
+                  ),
+                  
+                  ##### Plot Page #####
+                  tabItem(
+                    tabName = "Bar",
+                    fluidPage(
+                      box(
+                        title = "Select Feature",
+                        solidHeader = TRUE,
+                        status = "primary",
+                        width = 3,
+                        selectInput("bar_feature", label = "Feature", choices = colnames(dataPractice)),
+                        selectInput("bar_color", label = "Color", choices = colnames(dataPractice)),
+                        div(paste("▲ Color請選擇「屬性資料」"), style = "color:red")
+                      ),
+                      box(
+                        title = "Bar Chart",
+                        solidHeader = TRUE,
+                        status = "info",
+                        width = 9,
+                        plotOutput("bar_plot")
+                      )
+                    )
+                  ),
+                  tabItem(
+                    tabName = "Hist",
+                    fluidPage(
+                      box(
+                        title = "Select Feature",
+                        solidHeader = TRUE,
+                        status = "primary",
+                        width = 3,
+                        selectInput("hist_feature", label = "Feature", choices = colnames(dataPractice)),
+                        selectInput("hist_color", label = "Color", choices = colnames(dataPractice)),
+                        div(paste("▲ Color請選擇「屬性資料」"), style = "color:red")
+                      ),
+                      box(
+                        title = "Histogram",
+                        solidHeader = TRUE,
+                        status = "info",
+                        width = 9,
+                        plotOutput("hist_plot")
+                      )
+                    )
+                  ),
+                  tabItem(
+                    tabName = "Pie",
+                    fluidPage(
+                      box(
+                        title = "Select Feature",
+                        solidHeader = TRUE,
+                        status = "primary",
+                        width = 3,
+                        selectInput("pie_feature", label = "Feature", choices = colnames(dataPractice))
+                      ),
+                      box(
+                        title = "Pie Chart",
+                        solidHeader = TRUE,
+                        status = "info",
+                        width = 9,
+                        plotOutput("pie_plot")
+                      )
+                    )
+                  ),
+                  tabItem(
+                    tabName = "Scatter",
+                    fluidPage(
+                      box(
+                        title = "Select Feature",
+                        solidHeader = TRUE,
+                        status = "primary",
+                        width = 3,
+                        selectInput("scatter_x", label = "Var X", choices = colnames(dataPractice)),
+                        selectInput("scatter_y", label = "Var Y", choices = colnames(dataPractice)),
+                        selectInput("scatter_color", label = "Color", choices = colnames(dataPractice))
+                      ),
+                      box(
+                        title = "Scatter Plot",
+                        solidHeader = TRUE,
+                        status = "info",
+                        width = 9,
+                        plotOutput("scatter_plot")
+                      )
+                    )
+                  ),
+                  
+                  ##### Practice Page #####
+                  tabItem(
+                    tabName = "Practice",
+                    fluidPage(
+                      tabBox(
+                        width = 7,
+                        height = 1100,
+                        tabPanel(
+                          title = "練習實際案例操作",
+                          uiOutput("pdf5")
+                        )
+                      ),
+                      box(
+                        title = "R Code Practice",
+                        solidHeader = TRUE,
+                        status = "primary",
+                        width=5,
+                        height = 1100,
+                        htmlOutput("html2"))
+                    )
+                  ),
+                  
+                  #### Code Page #####
+                  tabItem(
+                    tabName = "Code",
+                    htmlOutput("jupyter")
+                  )
+                )
+              )
+)
+
